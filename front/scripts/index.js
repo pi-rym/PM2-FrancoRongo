3f590e65fad3ffd799ae1bf781feac2fb172e764
FrancoRongo <franco.rongo98@hotmail.com>
2024-03-11 21:20:39 -0300

    Homework Axios

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,6 +1,18 @@
+const axios = require("axios");
 const rendersCards = require("./rendersCards");
+const apiUrl= "https://students-api.up.railway.app/movies";
 
-$.get("https://students-api.2.us-1.fl0.io/movies",(data)=>{
-   rendersCards(data);
-})
+//$.get("https://students-api.2.us-1.fl0.io/movies",(data)=>{
+ //  rendersCards(data);
+//})
 
+axios.get(apiUrl)
+  .then(response => {
+     rendersCards(response.data);
+    
+    //console.log('Respuesta de la API:', response.data);
+  })
+   .catch(error => {
+    
+    console.error('Error al obtener datos:', error);
+  });
